02a5a7ad9701113000707475c2076c609b1646c1
Eddy Kim <eddy.soohwan.kim@gmail.com>
2024-03-28 13:08:47 -0700

    GAE Microservice Backend

---
+++ src/main/java/com/mihky/legoservice/LegoserviceApplication.java
@@ -0,0 +1,13 @@
+package com.mihky.legoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegoserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LegoserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mihky/legoservice/LegoserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mihky.legoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegoserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
